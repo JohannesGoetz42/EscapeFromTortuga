05e529ed5dee4ef322dfa8b487afef7fb32a4bc9
Johannes Götz <johannes.goetz@futuristen.education>
2024-08-08 10:58:15 +0200

    add basic movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float movementSpeed = 10.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 movementVector = Vector3.Normalize(new Vector3(horizontalInput, 0.0f, verticalInput));
+
+        transform.position += movementVector * Time.deltaTime * movementSpeed;
     }
 }

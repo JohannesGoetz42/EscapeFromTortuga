cf2f00926118b332b1e82a21bba90d3bae3a352d
Johannes Goetz <johannesgoetz1991@web.de>
2024-08-14 11:49:49 +0200

    implement model switch

---
+++ Assets/Scripts/ModelSwitch.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ModelSwitch : MonoBehaviour
+{
+    private List<GameObject> _availableModels = new List<GameObject>();
+
+    private void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.CompareTag("ModelOption"))
+            {
+                _availableModels.Add(child.gameObject);
+            }
+        }
+
+        SelectModel(GameManager.Instance.SelectedCharacter);
+    }
+
+    public void SelectNextModel()
+    {
+        SelectModel(GameManager.Instance.SelectedCharacter + 1);
+    }
+    public void SelectPreviousModel()
+    {
+        SelectModel(GameManager.Instance.SelectedCharacter - 1);
+    }
+
+    void SelectModel(int modelIndex)
+    {
+        modelIndex %= _availableModels.Count;
+        
+        // set the selected model active and all other available models inactive
+        for (int i = 0; i < _availableModels.Count; i++)
+        {
+            _availableModels[i].SetActive(modelIndex == i);
+        }
+
+        GameManager.Instance.SelectedCharacter = modelIndex;
+    }
+}

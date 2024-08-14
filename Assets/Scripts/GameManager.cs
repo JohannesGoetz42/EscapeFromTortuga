b159d5c49c3caf1ecb53f45d3b96e30313c72f4b
Johannes Götz <johannes.goetz@futuristen.education>
2024-08-14 12:05:00 +0200

    implement material switch

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 
     public string PlayerName;
     public int SelectedCharacter;
+    public int SelectedMaterial;
+    public Material[] AvailableMaterials;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
---
+++ Assets/Scripts/MaterialSwitch.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class MaterialSwitch : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SelectMaterial(GameManager.Instance.SelectedMaterial);
+    }
+
+    public void SelectNextMaterial()
+    {
+        SelectMaterial(GameManager.Instance.SelectedMaterial + 1);
+    }
+    public void SelectPreviousMaterial()
+    {
+        SelectMaterial(GameManager.Instance.SelectedMaterial - 1);
+    }
+
+    void SelectMaterial(int materialIndex)
+    {
+        materialIndex %= GameManager.Instance.AvailableMaterials.Length;
+
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.CompareTag("ModelOption"))
+            {
+                foreach (SkinnedMeshRenderer model in child.GetComponents<SkinnedMeshRenderer>())
+                {
+                    if (model != null)
+                    {
+                        model.material = GameManager.Instance.AvailableMaterials[materialIndex];
+                    }
+                }
+            }
+        }
+
+        GameManager.Instance.SelectedMaterial = materialIndex;
+    }
+}

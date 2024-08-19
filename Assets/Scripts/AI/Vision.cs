96c562b0a764947917c35e84305cc2c8530a3b24
Johannes Götz <johannes.goetz@futuristen.education>
2024-08-19 13:32:01 +0200

    visualize view cone

--- Assets/Scripts/AI/Vision.cs
+++ Assets/Scripts/AI/Vision.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
+using UnityEngine.Profiling;
 
 public class Vision : MonoBehaviour
 {
-    public float ViewAngle;
-    public float ViewRange;
+    public float viewAngle = 90.0f;
+    public float viewRange = 10.0f;
 
+    [SerializeField]
+    int visualizationSubdivisions = 10;
+
+    [SerializeField]
+    private MeshFilter viewConeFilter;
+    private Mesh viewConeVisualization;
     private NPCController npcController;
     private PlayerController playerController;
 
@@ -12,6 +19,14 @@ public class Vision : MonoBehaviour
     void Start()
     {
         npcController = GetComponent<NPCController>();
+        if (viewConeFilter != null)
+        {
+            viewConeVisualization = new Mesh();
+            viewConeVisualization.name = "View cone visualization";
+
+            viewConeFilter.mesh = viewConeVisualization;
+            UpdateVisualization();
+        }
     }
 
     // Update is called once per frame
@@ -32,7 +47,7 @@ public class Vision : MonoBehaviour
 
         Vector3 direction = target.position - transform.position;
         // target is out of view range
-        if (direction.sqrMagnitude > System.Math.Pow(ViewRange, 2))
+        if (direction.sqrMagnitude > System.Math.Pow(viewRange, 2))
         {
             return false;
         }
@@ -40,6 +55,45 @@ public class Vision : MonoBehaviour
         float dotProduct = Vector3.Dot(transform.forward, direction.normalized);
         float angle = Mathf.Acos(dotProduct) * Mathf.Rad2Deg;
 
-        return ViewAngle > angle;
+        return viewAngle > angle;
+    }
+
+    void UpdateVisualization()
+    {
+        Profiler.BeginSample("Update vision cone");
+        int requiredVectorCount = visualizationSubdivisions + 2;
+        Vector3[] vertices = new Vector3[requiredVectorCount];
+        Vector2[] uvs = new Vector2[requiredVectorCount];
+
+        int[] triangles = new int[visualizationSubdivisions * 3];
+
+        Vector3 stepDirection = Vector3.forward * viewRange;
+        stepDirection = Quaternion.AngleAxis(-viewAngle * 0.5f, Vector3.up) * stepDirection;
+        Quaternion stepRotation = Quaternion.AngleAxis(viewAngle / visualizationSubdivisions, Vector3.up);
+
+        // add the first two vertices at the source position
+        vertices[0] = new Vector3(0.0f, 0.1f, 0.0f);
+        vertices[1] = vertices[0] + stepDirection;
+        uvs[0] = Vector2.zero;
+        uvs[1] = Vector2.one;
+
+        // create all triangles
+        for (int i = 2; i < requiredVectorCount; i++)
+        {
+            stepDirection = stepRotation * stepDirection;
+
+            vertices[i] = vertices[0] + stepDirection;
+            uvs[i] = Vector2.one;
+
+            int trianglesStartIndex = (i - 2) * 3;
+            triangles[trianglesStartIndex] = 0;
+            triangles[trianglesStartIndex + 1] = i - 1;
+            triangles[trianglesStartIndex + 2] = i;
+        }
+
+        viewConeVisualization.vertices = vertices;
+        viewConeVisualization.triangles = triangles; // triangles must be set AFTER vertices!
+        viewConeVisualization.uv = uvs;
+        Profiler.EndSample();
     }
 }

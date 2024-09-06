df65834c0d0626c5a863fba1796a4d5271c68ad7
Johannes Goetz <johannesgoetz1991@web.de>
2024-09-06 12:41:23 +0200

    add wait time action

---
+++ Assets/Scripts/AI/BehaviorTrees/Actions/WaitTimeAction.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitTimeAction : BehaviorTreeAction
+{
+    [field: SerializeField]
+    public BlackboardKeySelector blackboardTime {  get; set; }
+    [SerializeField]
+    bool useBlackboardTime = false;
+    [SerializeField]
+    float WaitForSeconds = 5.0f;
+
+    Dictionary<IBehaviorTreeUser, float> _endTimesMemory = new Dictionary<IBehaviorTreeUser, float>();
+
+    public WaitTimeAction() : base()
+    {
+#if UNITY_EDITOR
+        nodeName = "Wait time";
+#endif
+
+        blackboardTime = new BlackboardKeySelector(BlackboardValueType.Float, new BlackboardValueType[] { BlackboardValueType.Float });
+    }
+
+    protected override void OnBecomeRelevant(IBehaviorTreeUser user)
+    {
+        float waitTime = useBlackboardTime ? user.GetBlackboard().GetValueAsFloat(blackboardTime.selectedKey) : WaitForSeconds;
+        if (_endTimesMemory.ContainsKey(user))
+        {
+            _endTimesMemory[user] = Time.time + waitTime;
+        }
+        else
+        {
+            _endTimesMemory.Add(user, Time.time + waitTime);
+        }
+    }
+
+    protected override void OnUpdate(IBehaviorTreeUser user)
+    {
+        if (Time.time > _endTimesMemory[user])
+        {
+            Exit(user, BehaviorNodeState.Success);
+        }
+    }
+}

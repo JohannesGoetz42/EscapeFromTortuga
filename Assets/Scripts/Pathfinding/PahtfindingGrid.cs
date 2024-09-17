b1f1ba4ab95381223fc44eadf21ce5116eab22ff
Johannes Goetz <johannesgoetz1991@web.de>
2024-09-17 14:11:41 +0200

    consider landscape height in pathing grid

--- Assets/Scripts/Pathfinding/PahtfindingGrid.cs
+++ Assets/Scripts/Pathfinding/PahtfindingGrid.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 
 public class PathfindingGrid : MonoBehaviour
 {
-    public LayerMask unwalkableMask;
-    public Vector2 gridWorldSize;
-    public float nodeRadius;
+    [SerializeField]
+    LayerMask unwalkableMask;
+    [SerializeField]
+    LayerMask walkableMask;
+    [SerializeField]
+    Vector3 gridWorldSize;
+    [SerializeField]
+    float nodeRadius;
 
     PathfindingNode[,] grid;
     float nodeDiameter;
@@ -19,7 +24,7 @@ public class PathfindingGrid : MonoBehaviour
         Vector3 relativePosition = worldPosition - transform.localPosition;
 
         float coordX = (relativePosition.x + gridWorldSize.x * 0.5f) / gridWorldSize.x;
-        float coordY = (relativePosition.z + gridWorldSize.y * 0.5f) / gridWorldSize.y;
+        float coordY = (relativePosition.z + gridWorldSize.z * 0.5f) / gridWorldSize.z;
         coordX = Mathf.Clamp(coordX, 0.0f, 1.0f);
         coordY = Mathf.Clamp(coordY, 0.0f, 1.0f);
 
@@ -58,7 +63,7 @@ public class PathfindingGrid : MonoBehaviour
     {
         nodeDiameter = nodeRadius * 2;
         gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
-        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.z / nodeDiameter);
         CreateGrid();
     }
 
@@ -66,9 +71,10 @@ public class PathfindingGrid : MonoBehaviour
     {
         grid = new PathfindingNode[gridSizeX, gridSizeY];
 
-        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.z / 2;
         float diameterPlusRadius = nodeDiameter + nodeRadius;
 
+        float maxFloorHeight = gridWorldSize.y * 0.5f + transform.position.y;
         // create nodes for every row
         for (int x = 0; x < gridSizeX; x++)
         {
@@ -76,7 +82,22 @@ public class PathfindingGrid : MonoBehaviour
             for (int y = 0; y < gridSizeY; y++)
             {
                 Vector3 nodePosition = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
-                bool walkable = !Physics.CheckSphere(nodePosition, nodeRadius, unwalkableMask);
+
+
+                // try to find a floor
+                Vector3 rayStart = nodePosition;
+                rayStart.y = maxFloorHeight;
+
+                RaycastHit floorHit;
+                bool walkable = Physics.Raycast(rayStart, Vector3.down, out floorHit, gridWorldSize.y, walkableMask);
+                nodePosition = floorHit.point;
+
+                // check if obstructed by unwalkable object
+                if (walkable)
+                {
+                    walkable = !Physics.CheckSphere(nodePosition, nodeRadius, unwalkableMask);
+                }
+
                 grid[x, y] = new PathfindingNode(walkable, nodePosition, x, y);
             }
         }
@@ -85,7 +106,16 @@ public class PathfindingGrid : MonoBehaviour
 #if UNITY_EDITOR
     void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+        Gizmos.DrawWireCube(transform.position, gridWorldSize);
+
+        if (grid != null)
+        {
+            foreach (PathfindingNode node in grid)
+            {
+                Gizmos.color = node.isWalkable ? Color.white : Color.red;
+                Gizmos.DrawCube(node.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+            }
+        }
     }
 #endif
 }

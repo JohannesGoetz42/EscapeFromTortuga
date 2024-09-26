20895b99f6aafbb7bffe81a07a11993ed0140e7e
Johannes Goetz <johannesgoetz1991@web.de>
2024-09-26 13:10:04 +0200

    fix camera control

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,21 +2,23 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    private Vector3 offset;
+    [SerializeField]
+    private float cameraDistance = 30.0f;
+    [SerializeField]
+    private float cameraAngle = 70.0f;
 
-    private void Start()
-    {
-        if (PlayerController.Instance)
-        {
-            offset = transform.position - PlayerController.Instance.transform.position;
-        }
-    }
+    public Vector3 cameraOffset { get; private set; }
 
     void LateUpdate()
     {
         if (PlayerController.Instance)
         {
-            transform.position = offset + PlayerController.Instance.transform.position;
+            Quaternion cameraRotation = Quaternion.Euler(cameraAngle, 0.0f, 0.0f);
+            cameraOffset = Vector3.up * cameraDistance;
+            cameraOffset = Quaternion.Euler(-90.0f, 0.0f, 0.0f) * (cameraRotation * cameraOffset);
+
+            transform.position = PlayerController.Instance.transform.position + cameraOffset;
+            transform.rotation = cameraRotation;
         }
     }
 }
